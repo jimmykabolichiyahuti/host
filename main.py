a66679648d075787ab451c61c2fa0dccc3f759fa
justin xie <108196305+jimmykabolichiyahuti@users.noreply.github.com>
2026-03-31 00:41:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+import stripe
+load_dotenv()
+stripe.api_key = os.getenv("stripe_secretkey")
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://whitecapsclo.com"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+def home():
+    return {"status": "ok"}
+class PaymentRequest(BaseModel):
+    price_id: str
+@app.post("/create-payment-intent")
+def create_payment_intent(gg: PaymentRequest):
+    try:
+        price = stripe.Price.retrieve(gg.price_id)
+
+        intent = stripe.PaymentIntent.create(
+            amount=price.unit_amount,
+            currency=price.currency,
+            automatic_payment_methods={"enabled": True},
+        )
+        return {"client_secret": intent.client_secret}
+    except stripe.error.StripeError as e:
+        raise HTTPException(status_code=400, detail=str(e))
